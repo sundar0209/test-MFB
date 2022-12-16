0553ef91c71ac54a03d161723f7310a439172b69
sundar0209 <82659930+sundar0209@users.noreply.github.com>
2022-12-16 17:04:04 +0530

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #give permission for everything in the express-app directory
-sudo chmod -R 777 /var/www/html/sundar
+sudo chmod -R 755 /var/www/html/sundar
 
 #navigate into our working directory where we have all our github files
 cd /var/www/html/sundar
@@ -12,7 +12,17 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
 #install node modules
-npm install
+npm install --unsafe --perm
+
+#npm take build 
+npm run build 
+cd build 
+mv * /var/www/html/sundar
+cd ../
 
 #start our node app in the background
-node app.js > app.out.log 2> app.err.log < /dev/null & 
+#node app.js > app.out.log 2> app.err.log < /dev/null & 
+
+#start ui in linux server .
+serve -s sundar &>> out.log &
+
